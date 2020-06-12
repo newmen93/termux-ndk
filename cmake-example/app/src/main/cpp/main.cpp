cdc8d58c9caee4c51b7367d5facb1dc6a6b519ba
Lzhiyong <975434530@qq.com>
2020-06-13 07:31:42 +0800

    update app

--- cmake-example/app/src/main/cpp/main.cpp
+++ cmake-example/app/src/main/cpp/main.cpp
@@ -1,7 +1,7 @@
 #include <jni.h>
 #include <android/log.h>
 
-#define LOG_TAG "EXAMPLE"
+#define LOG_TAG "CMAKE-EXAMPLE"
 
 extern "C" {
     
@@ -9,7 +9,7 @@ extern "C" {
 JNIEXPORT jstring JNICALL 
 Java_com_cmake_example_MainActivity_stringFromJNI(JNIEnv* env, jobject thiz) {
     
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "hello\n");
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "cmake-example");
     
     return env->NewStringUTF("Hello Android!");
     
