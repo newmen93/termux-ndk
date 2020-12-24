35b0db80ff2f8e2b7f927ee00d982719d58cd406
Lzhiyong <975434530@qq.com>
2020-12-24 14:10:31 +0800

    update

--- openjdk-build/tls-test/main.cpp
+++ openjdk-build/tls-test/main.cpp
@@ -4,7 +4,7 @@
 #include "thread.h"
 #include "JavaThread.h"
 
-// test __thread if works
+// test __thread if works?
 __thread int var = 5;
 
 void* worker1(void* arg){
@@ -21,11 +21,10 @@ void* worker2(void* arg){
 int main(int argc, char *argv[]){
 
     Thread mThread;
-
     JavaThread jThread;
 
     printf("mThread addr: %p\n", mThread.get_thread_current());
-
+    
     printf("jThread addr: %p\n", JavaThread::aarch64_get_thread_helper());
 
     pthread_t pid1, pid2;
@@ -37,6 +36,7 @@ int main(int argc, char *argv[]){
     pthread_join(pid2, NULL);
 
     printf("android tls test success!\n");
+    
     return 0;
 }
 
